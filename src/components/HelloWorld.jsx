c5ac43f9fcdefa7200fff19014ab3b353b0b943e
Victor Riveros <vriveros@illinois.edu>
2021-07-19 15:23:12 -0500

    components header and search added

--- src/components/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss'
-
-const HelloWorld = ()=>(
-    <h1>hola mundo</h1>
-)
-
-export default HelloWorld;
---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/App.scss'
+import Header from '../components/Header';
+import Search from '../components/Search';
+
+
+const App = () => (
+    <div className="App">
+        <Header />
+        <Search />
+    </div>
+);
+
+export default App;
